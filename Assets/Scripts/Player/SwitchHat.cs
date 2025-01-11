2d86fc76ded9351717d6c2c3c98ad714b9951f72
Juan Pablo Martínez <110469451+PabloH5@users.noreply.github.com>
2025-01-11 14:40:24 -0500

    feat: create logic for switch hats from enum

--- Assets/Scripts/Player/SwitchHat.cs
+++ Assets/Scripts/Player/SwitchHat.cs
@@ -4,27 +4,65 @@ using UnityEngine;
 
 public class SwitchHat : MonoBehaviour
 {
+    public enum CurrentHat
+    {
+        elegant,
+        picnic,
+        chinese,
+        mexican,
+        pirate,
+        none
+    }
 
+    public CurrentHat currentHat;
     [SerializeField] private List<GameObject> hatList;
 
-    private enum CurrentHat
+
+    void FixedUpdate()
     {
-        mexican,
-        straw,
-        elegant
+        SwitchCurretHat();
     }
 
-    [SerializeField] private CurrentHat currentHat;
-
-    // Start is called before the first frame update
-    void Start()
+    private void SwitchCurretHat()
     {
-        
+        TurnOffHats();  
+        switch (currentHat)
+        {
+            case CurrentHat.elegant:
+                hatList[0].SetActive(true);
+                break;
+
+            case CurrentHat.picnic:
+                hatList[1].SetActive(true);
+                break;
+
+            case CurrentHat.chinese:
+                hatList[2].SetActive(true);
+                break;
+
+            case CurrentHat.mexican:
+                hatList[3].SetActive(true);
+                break;
+
+            case CurrentHat.pirate:
+                hatList[4].SetActive(true);
+                break;
+            
+            case CurrentHat.none:
+                TurnOffHats(); 
+                break;
+
+            default:
+                TurnOffHats();  
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TurnOffHats()
     {
-        
+        foreach (var item in hatList)
+        {
+            item.SetActive(false);
+        }
     }
 }
